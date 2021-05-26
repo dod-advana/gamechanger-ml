e7f2ffac983d90c84cae95a1bb64b8d52eef6668
Erin <>
2021-05-26 14:57:31 -0400

    saving updated dataset file

--- gamechangerml/src/search/sent_transformer/dataset.py
+++ gamechangerml/src/search/sent_transformer/dataset.py
@@ -1,4 +1,6 @@
-class GCDataset(TensorDataset):
+from gamechangerml.src.search.sent_transformer.corpus import SentenceCorpus
+
+class GCDataset():
     def __init__(self, fpath, iter_len = 1_000):
         self.corp = SentenceCorpus(fpath, iter_len = iter_len)
         self.iter_len = iter_len
@@ -6,7 +8,11 @@ class GCDataset(TensorDataset):
     def __len__(self):
         return self.iter_len
 
-    def __getitem__(self, index):
-        print("hi")
+    def getitem(self):
         text_a, text_b, sim_score = self.corp._get_item_sample()
-        return InputExample(texts=[text_a[0], text_b[0]], label=float(sim_score/9))
+        return text_a[0], text_b[0]], float(sim_score/9)
+
+
+if __name__ == "__main__":
+    dataset = GCDataset("../parsed good")
+    print(dataset.getitem())
