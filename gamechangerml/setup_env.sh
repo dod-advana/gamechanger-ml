5845456554903a93bcb17be4903f4929416d0b3a
Eddie Storm <vctrstrm@users.noreply.github.com>
2021-07-14 23:47:05 -0700

    Refactoring setup_env.sh script for clarity and compatibility with k8s deployment modes

--- gamechangerml/setup_env.sh
+++ gamechangerml/setup_env.sh
@@ -1,95 +1,110 @@
-#!/bin/bash
-if [ -z $1 ]
-then 
-  echo "No dev environment specified, setting to PROD"
-  export ENV_TYPE="PROD"
-elif [ "$1" = "DEV" ]
-then
-  export ENV_TYPE="DEV"
-elif [ "$1" = "PROD" ]
-then
-  export ENV_TYPE="PROD"
-elif [ "$1" = "DEVLOCAL" ]
-then
-  export ENV_TYPE="DEVLOCAL"
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
 
-fi 
+ENV_TYPE="${1:-}"
+DOWNLOAD_DEP="${2:-}"
 
-if [ "$ENV_TYPE" = "PROD" ]
-then
-  echo "Setting up PROD environment"
-  export TRANSFORMER_HOST=http://localhost
-  export REDIS_HOST=localhost
-  export REDIS_PORT=6379
-  export GC_ML_HOST=http://localhost
-  S3_TRANS_MODEL_PATH="${S3_TRANS_MODEL_PATH:=s3://advana-raw-zone/gamechanger/models/transformers/v5/transformers.tar.gz}"
-  export S3_TRANS_MODEL_PATH
-  S3_SENT_INDEX_PATH="${S3_SENT_INDEX_PATH:=s3://advana-raw-zone/gamechanger/models/sentence_index/v4/sent_index_20210422.tar.gz}"
-  export S3_SENT_INDEX_PATH
-  S3_QEXP_PATH="${S3_QEXP_PATH:=s3://advana-raw-zone/gamechanger/models/qexp_model/v3/qexp_20201217.tar.gz}"
-  export S3_QEXP_PATH
-  S3_TOPICS_PATH="${S3_TOPICS_PATH:=s3://advana-raw-zone/gamechanger/models/topic_model/v1/20210208.tar.gz}"
-  export S3_TOPICS_PATH
+[[ -z "${ENV_TYPE}" ]] && {
+  >&2 echo "[WARNING] [SETUP_ENV SCRIPT] No ENV_TYPE - 1st arg - specified, setting to 'PROD' ..."
+  ENV_TYPE="PROD"
+}
 
-  export DEV_ENV="PROD"
-fi
+function setup_prod() {
+    echo "[INFO] Setting up PROD environment ..."
+    export TRANSFORMER_HOST="${TRANSFORMER_HOST:-http://localhost}"
+    export REDIS_HOST="${REDIS_HOST:-localhost}"
+    export REDIS_PORT="${REDIS_PORT:-6379}"
+    export GC_ML_HOST="${GC_ML_HOST:-http://localhost}"
+    export S3_TRANS_MODEL_PATH="${S3_TRANS_MODEL_PATH:-s3://advana-raw-zone/gamechanger/models/transformers/v5/transformers.tar.gz}"
+    export S3_SENT_INDEX_PATH="${S3_SENT_INDEX_PATH:-s3://advana-raw-zone/gamechanger/models/sentence_index/v4/sent_index_20210422.tar.gz}"
+    export S3_QEXP_PATH="${S3_QEXP_PATH:-s3://advana-raw-zone/gamechanger/models/qexp_model/v3/qexp_20201217.tar.gz}"
+    export S3_TOPICS_PATH="${S3_TOPICS_PATH:-s3://advana-raw-zone/gamechanger/models/topic_model/v1/20210208.tar.gz}"
 
+    export DEV_ENV="PROD"
+}
 
-if [ "$ENV_TYPE" = "DEV" ]
-then
-  echo "Setting up DEV Docker environment"
-  export REDIS_HOST=gc-redis
-  export REDIS_PORT=6380
-  export GC_ML_HOST=http://host.docker.internal
-  S3_TRANS_MODEL_PATH="${S3_TRANS_MODEL_PATH:=s3://advana-raw-zone/gamechanger/models/transformers/v5/transformers.tar.gz}"
-  export S3_TRANS_MODEL_PATH
-  S3_SENT_INDEX_PATH="${S3_SENT_INDEX_PATH:=s3://advana-raw-zone/gamechanger/models/sentence_index/v4/sent_index_20210422.tar.gz}"
-  export S3_SENT_INDEX_PATH
-  S3_QEXP_PATH="${S3_QEXP_PATH:=s3://advana-raw-zone/gamechanger/models/qexp_model/v3/qexp_20201217.tar.gz}"
-  export S3_QEXP_PATH
+function setup_dev() {
+    echo "[INFO] Setting up DEV Docker environment ..."
+    export REDIS_HOST="${REDIS_HOST:-gc-redis}"
+    export REDIS_PORT="${REDIS_PORT:-6380}"
+    export GC_ML_HOST="${GC_ML_HOST:-http://host.docker.internal}"
+    export S3_TRANS_MODEL_PATH="${S3_TRANS_MODEL_PATH:-s3://advana-raw-zone/gamechanger/models/transformers/v5/transformers.tar.gz}"
+    export S3_SENT_INDEX_PATH="${S3_SENT_INDEX_PATH:-s3://advana-raw-zone/gamechanger/models/sentence_index/v4/sent_index_20210422.tar.gz}"
+    export S3_QEXP_PATH="${S3_QEXP_PATH:-s3://advana-raw-zone/gamechanger/models/qexp_model/v3/qexp_20201217.tar.gz}"
+    export S3_TOPICS_PATH="${S3_TOPICS_PATH:-s3://advana-raw-zone/gamechanger/models/topic_model/v1/20210208.tar.gz}"
 
-  S3_TOPICS_PATH="${S3_TOPICS_PATH:=s3://advana-raw-zone/gamechanger/models/topic_model/v1/20210208.tar.gz}"
-  export S3_TOPICS_PATH
-
-  if [ -z "$AWS_PROFILE" ]
-  then
-      echo "\$AWS_PROFILE is empty"
-      unset AWS_PROFILE
-  fi
-  if [ -z "$AWS_DEFAULT_PROFILE" ]
-  then
-      echo "\$AWS_DEFAULT_PROFILE is empty"
-      unset AWS_DEFAULT_PROFILE
-  fi
   export DEV_ENV="DEV"
-  export PULL_MODELS="latest"
+  export PULL_MODELS="${PULL_MODELS:-latest}"
+  export MLFLOW_HOST="${MLFLOW_HOST:-localhost}"
   export MLFLOW_TRACKING_URI="http://${MLFLOW_HOST}:5050/"
-fi
+}
 
-if [ "$ENV_TYPE" = "DEVLOCAL" ]
-then
-  echo "Setting up DEV environment"
-  export REDIS_HOST=localhost
-  export REDIS_PORT=6380
-  export GC_ML_HOST=http://localhost
-  S3_TRANS_MODEL_PATH="${S3_TRANS_MODEL_PATH:=s3://advana-raw-zone/gamechanger/models/transformers/v5/transformers.tar.gz}"
-  export S3_TRANS_MODEL_PATH
-  S3_SENT_INDEX_PATH="${S3_SENT_INDEX_PATH:=s3://advana-raw-zone/gamechanger/models/sentence_index/v2/sent_index_20210223.tar.gz}"
-  export S3_SENT_INDEX_PATH
+
+function setup_devlocal() {
+  echo "[INFO] Setting up DEVLOCAL environment ..."
+  export REDIS_HOST="${REDIS_HOST:-localhost}"
+  export REDIS_PORT="${REDIS_PORT:-6380}"
+  export GC_ML_HOST="${GC_ML_HOST:-http://localhost}"
+  export S3_TRANS_MODEL_PATH="${S3_TRANS_MODEL_PATH:-s3://advana-raw-zone/gamechanger/models/transformers/v5/transformers.tar.gz}"
+  export S3_SENT_INDEX_PATH="${S3_SENT_INDEX_PATH:-s3://advana-raw-zone/gamechanger/models/sentence_index/v2/sent_index_20210223.tar.gz}"
 
   export DEV_ENV="DEVLOCAL"
-fi
+}
 
-echo "ENVIRONMENT SET: ${DEV_ENV:-} "
-echo " * AWS SETTING: ${DEV_ENV:-} "
-echo " * TRANSFORMER HOSTNAME: ${TRANSFORMER_HOST:-} "
-echo " * REDIS HOST: ${REDIS_HOST:-} "
-echo " * FLASK HOST: ${GC_ML_HOST:-} "
-echo " * REDIS PORT: ${REDIS_PORT:-} "
-echo " * PULL MODELS: ${PULL_MODELS:-} "
-echo " * GC_ML_API_MODEL_NAME: ${GC_ML_API_MODEL_NAME:-} "
-echo " * S3_TRANS_MODEL_PATH: ${S3_TRANS_MODEL_PATH:-}"
-echo " * S3_SENT_INDEX_PATH: ${S3_SENT_INDEX_PATH:-}"
-echo " * S3_QEXP_PATH: ${S3_QEXP_PATH:-}"
-echo " * S3_TOPICS_PATH: ${S3_TOPICS_PATH:-}"
+function setup_k8s_dev() {
+  setup_dev
+}
+
+function setup_k8s_test() {
+  setup_dev
+}
+
+function setup_k8s_prod() {
+  setup_prod
+}
+
+# if set to blank, unset
+[[ -z "${AWS_PROFILE:-}" ]] && unset AWS_PROFILE
+[[ -z "${AWS_DEFAULT_PROFILE:-}" ]] && unset AWS_DEFAULT_PROFILE
+
+case "$ENV_TYPE" in
+  PROD)
+    setup_prod
+    ;;
+  DEV)
+    setup_dev
+    ;;
+  DEVLOCAL)
+    setup_devlocal
+    ;;
+  K8S_DEV)
+    setup_k8s_dev
+    ;;
+  K8S_TEST)
+    setup_k8s_test
+    ;;
+  K8s_PROD)
+    setup_k8s_prod
+    ;;
+  *)
+    >&2 echo "[ERROR] Invalid ENV_TYPE specified: '$ENV_TYPE'"
+    ;;
+esac
+
+cat <<EOF
+ENVIRONMENT SET: ${DEV_ENV:-<unset>} "
+  * AWS SETTING: ${DEV_ENV:-<unset>} "
+  * TRANSFORMER HOSTNAME: ${TRANSFORMER_HOST:-<unset>} "
+  * REDIS HOST: ${REDIS_HOST:-<unset>} "
+  * FLASK HOST: ${GC_ML_HOST:-<unset>} "
+  * REDIS PORT: ${REDIS_PORT:-<unset>} "
+  * PULL MODELS: ${PULL_MODELS:-<unset>} "
+  * GC_ML_API_MODEL_NAME: ${GC_ML_API_MODEL_NAME:-<unset>} "
+  * S3_TRANS_MODEL_PATH: ${S3_TRANS_MODEL_PATH:-<unset>}"
+  * S3_SENT_INDEX_PATH: ${S3_SENT_INDEX_PATH:-<unset>}"
+  * S3_QEXP_PATH: ${S3_QEXP_PATH:-<unset>}"
+  * S3_TOPICS_PATH: ${S3_TOPICS_PATH:-<unset>}"
+EOF
 
