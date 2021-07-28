84ce3d98c21254e7e7e7241c49b00ea0f65d13bb
604840 <rha930@gmail.com>
2021-07-28 16:13:07 -0400

    adding try except for process maanger so you dont need redis

--- gamechangerml/api/utils/processmanager.py
+++ gamechangerml/api/utils/processmanager.py
@@ -11,53 +11,59 @@ training = "training: train_model"
 reloading = "models: reloading_models"
 
 # the dictionary that holds all the progress values
-PROCESS_STATUS = CacheVariable("process_status", True)
-COMPLETED_PROCESS = CacheVariable("completed_process", True)
-thread_lock = threading.Lock()
+try:
+    PROCESS_STATUS = CacheVariable("process_status", True)
+    COMPLETED_PROCESS = CacheVariable("completed_process", True)
+    thread_lock = threading.Lock()
+    default_flags = {
+        corpus_download: False,
+        clear_corpus: False,
+        training: False,
+        loading_corpus:False,
+        reloading:False
+    }
 
-default_flags = {
-    corpus_download: False,
-    clear_corpus: False,
-    training: False,
-    loading_corpus:False,
-    reloading:False
-}
-
-PROCESS_STATUS.value = {
-    "flags":default_flags
-}
-COMPLETED_PROCESS.value = []
+    PROCESS_STATUS.value = {
+        "flags":default_flags
+    }
+    COMPLETED_PROCESS.value = []
+except Exception as e:
+    print(e)
 
 
 def update_status(key, progress = 0, total = 100, failed = False):
-    if progress == total or failed:
-        date = datetime.now()
-        date_string = date.strftime("%Y-%m-%d %H:%M:%S")
-        completed = {
-            "process": key,
-            "total": total,
-            "date": date_string
-        }
-        with thread_lock:
-            if key in PROCESS_STATUS.value:
-                temp = PROCESS_STATUS.value
-                temp.pop(key, None)
-                temp["flags"][key] = False
-                PROCESS_STATUS.value = temp
-            if not failed:
-                completed_list = COMPLETED_PROCESS.value
-                completed_list.append(completed)
-                COMPLETED_PROCESS.value = completed_list
-    else:
-        status = {
-            "progress":progress,
-            "total":total
-        }
-        with thread_lock:
-            status_dict = PROCESS_STATUS.value
-            status_dict[key] = status
-            status_dict["flags"][key] = True
-            PROCESS_STATUS.value = status_dict
+
+    try:
+        if progress == total or failed:
+            date = datetime.now()
+            date_string = date.strftime("%Y-%m-%d %H:%M:%S")
+            completed = {
+                "process": key,
+                "total": total,
+                "date": date_string
+            }
+            with thread_lock:
+                if key in PROCESS_STATUS.value:
+                    temp = PROCESS_STATUS.value
+                    temp.pop(key, None)
+                    temp["flags"][key] = False
+                    PROCESS_STATUS.value = temp
+                if not failed:
+                    completed_list = COMPLETED_PROCESS.value
+                    completed_list.append(completed)
+                    COMPLETED_PROCESS.value = completed_list
+        else:
+            status = {
+                "progress":progress,
+                "total":total
+            }
+            with thread_lock:
+                status_dict = PROCESS_STATUS.value
+                status_dict[key] = status
+                status_dict["flags"][key] = True
+                PROCESS_STATUS.value = status_dict
+    except Exception as e:
+        print(e)
 
 def set_flags(key, value):
     with thread_lock:
