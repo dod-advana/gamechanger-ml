3758ad5e16c436d8aa19669b8882b385801f4a8c
cskiz <skiscim_chris@bah.com>
2021-07-29 17:38:57 -0400

    more TODOs

--- gamechangerml/src/entity/ner_training_data.py
+++ gamechangerml/src/entity/ner_training_data.py
@@ -47,7 +47,7 @@ def wc(txt):
 
 
 # TODO exclude abbreviations if it's part of an entity? e.g.,
-# TODO "The Director, DLA shall:". This will be tagged as
+# TODO "The Director, DLA shall:" will be tagged as
 # TODO  O GCPER GCPER GCORG-ABBRV O O
 def _gen_ner_conll_tags(abbrv_re, ent_re, entity2type, sent_dict, nlp):
     I_PRFX = "I-"
