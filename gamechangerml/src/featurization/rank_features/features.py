42cf7744e5a101d1afc5ac1a5a2413270591934b
Kate Dowdy <dowdy_katherine@bah.com>
2021-11-19 10:01:50 -0500

    Removing unused features

--- gamechangerml/src/featurization/rank_features/features.py
+++ gamechangerml/src/featurization/rank_features/features.py
@@ -9,8 +9,7 @@ pop_df = pd.read_csv(os.path.join(DATA_PATH, "features/popular_documents.csv"))
 
 """ retrieve pre-generated features from corpus
     - pr: pagerank
-    - orgs: organization importance
-    - kw_in_doc_score: keyword in doc score historically 
+    - pop_score: document popularity score 
 """
 df.orgs.replace({"'": '"'}, regex=True, inplace=True)
 
@@ -29,26 +28,3 @@ def get_pop_score(docId: str) -> float:
         return float(pop_df[pop_df.doc == docId].pop_score.values[0])
     else:
         return 0.0
-
-## TODO: R4EMOVE THESE???
-def get_orgs(docId: str) -> dict:
-    if docId in list(df.id):
-        return ast.literal_eval(df[df.id == docId].orgs.values[0])
-    else:
-        return {}
-
-
-def get_kw_score(docId: str) -> float:
-    if docId in list(df.id):
-        kw_score = df[df.id == docId].kw_in_doc_score.values[0]
-        if pd.isnull(kw_score):
-            return rank_min
-    else:
-        return rank_min
-
-
-def get_txt_length(docId: str) -> float:
-    if docId in list(df.id):
-        return df[df.id == docId].text_length.values[0]
-    else:
-        return rank_min
