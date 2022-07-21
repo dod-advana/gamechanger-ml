3d3a6978d8f3427949258c759e7ead9cd37bf812
Olivia Kroop <kroop_olivia@bah.com>
2022-07-21 13:25:13 -0700

    clean up generate_ft.py gamechangerml/src/featurization/rank_features/generate_ft.py - rm unused imports, simplify & organize imports - rm commented out code - use configure_logger() - autoformatting - refactor generate_pop_docs():     - take prod data as param instead of pop_kw_df     - normalize `kw_in_doc_score` here instead of in `generate_ft_doc()`     - add docstring - refactor/ simplify generate_ft_doc() (part 1):     - rm commented out code     - rm unused vars     - only load corpus data one time (was loaded twice)     - add check for required dict keys     - add comments & docstring - load & prepare corpus data with new create_corpus()

--- gamechangerml/src/featurization/rank_features/generate_ft.py
+++ gamechangerml/src/featurization/rank_features/generate_ft.py
@@ -1,61 +1,81 @@
-from gamechangerml.src.text_handling.process import preprocess
-import numpy as np
-import re
-from gamechangerml.src.featurization.rank_features import search_data as meta
-from gamechangerml.src.featurization.rank_features import rank
-from gamechangerml import DATA_PATH
-import datetime
-import pandas as pd
+from os.path import join
+from pandas import DataFrame, read_csv
 from tqdm import tqdm
-import argparse
-import logging
-import os
+from collections import Counter
+from argparse import ArgumentParser
+from glob import glob
+import en_core_web_md
+from gamechangerml.src.utilities import configure_logger, load_json
+from gamechangerml.src.text_handling.process import preprocess
+from gamechangerml.src.featurization.rank_features import search_data as meta
+from gamechangerml.src.featurization.rank_features.rank import Rank
+from gamechangerml import DATA_PATH
+
 
 """
 Usage:
     example:
     python -m gamechangerml.src.featurization.rank_features.generate_ft -c test/small_corpus/ -dd 80 --prod gamechangerml/src/search/ranking/generated_files/prod_test_data.csv
 
-optional arguements:
+optional arguments:
     --corpus, -c Corpus directory
     --days -dd days since today to get data
 """
 
 
-logger = logging.getLogger("gamechanger")
-
+logger = configure_logger()
 corpus_dir = "test/corpus_new"
-prod_data_file = os.path.join(
+prod_data_file = join(
     DATA_PATH, "features", "generated_files", "prod_test_data.csv"
 )
 
 
-def generate_pop_docs(pop_kw_df: pd.DataFrame, corpus_df: pd.DataFrame) -> pd.DataFrame:
-    """generate popular documents based on keyword searches
+def generate_pop_docs(prod_df: DataFrame, corpus_df: DataFrame) -> DataFrame:
+    """Create a DataFrame of corpus documents that are popular based on prod
+    searches.
+
     Args:
-        pop_kw_df: dataframe of keywords and counts
-        corpus_df: dataframe of corpus unique ID with text
+        prod_df (pandas.DataFrame): DataFrame of prod search data. Expected to
+            have the column `search` with str items. To include multiple values
+            in an item of the `search` column, the item can be a string of
+            values combined by ` AND | OR`.
+        corpus_df (pandas.DataFrame): DataFrame of corpus documents. Expected
+            to contain the columns `id` and `keywords`.
+
     Returns:
-        dataframe
-
+        DataFrame: DataFrame with columns `id`, `keywords`, and
+            `kw_in_doc_score`.
     """
+    kw_df = meta.get_top_keywords(prod_df)
+    docs = []
 
-    docList = []
-    for row_kw in tqdm(pop_kw_df.itertuples()):
+    for row_kw in tqdm(kw_df.itertuples()):
         for row_corp in corpus_df.itertuples():
             if len(row_corp.keywords):
                 if row_kw.keywords in row_corp.keywords[0]:
-                    docList.append(
-                        {"id": row_corp.id, "keywords": row_kw.keywords})
-    docDf = pd.DataFrame(docList, columns=["id", "keywords"])
-    docCounts = docDf.groupby("id").count().sort_values(
-        "keywords", ascending=False)
-    docCounts.rename(columns={"keywords": "kw_in_doc_score"}, inplace=True)
+                    docs.append(
+                        {"id": row_corp.id, "keywords": row_kw.keywords}
+                    )
+    docs_df = DataFrame(docs, columns=["id", "keywords"])
+    counts_df = (
+        docs_df.groupby("id").count().sort_values("keywords", ascending=False)
+    )
 
-    return docCounts
+    max_ = counts_df["keywords"].max()
+    min_ = counts_df["keywords"].min()
+    scores = [
+        (score - min_) / (max_ - min_) if score != 0 else 0.00001
+        for score in list(counts_df["keywords"])
+    ]
+    counts_df["keywords"] = scores
+    counts_df.rename(columns={"keywords": "kw_in_doc_score"}, inplace=True)
+
+    return counts_df
 
 
-def generate_ft_doc(corpus_dir: str, days: int = 80, prod_data: str = prod_data_file):
+def generate_ft_doc(
+    corpus_dir: str, days: int = 80, prod_data: str = prod_data_file
+):
     """generate feature document
     Args:
         corpus_dir: corpus directory
@@ -63,55 +83,127 @@ def generate_ft_doc(corpus_dir: str, days: int = 80, prod_data: str = prod_data_
     Returns:
 
     """
-    today = datetime.datetime.now()
-    out_dir = os.path.join(
-        DATA_PATH, "features", "generated_files"
+    out_dir = join(DATA_PATH, "features", "generated_files")
+
+    # Until we get connection to prod data, load prod data from csv.
+    logger.info(f"Reading in prod data from {prod_data}")
+    prod_df = read_csv(prod_data)
+    logger.info("Getting top keywords")
+
+    corpus_docs = create_corpus(corpus_dir, logger)
+    corp_df = DataFrame(corpus_docs)
+
+    # Calculate pagerank scores and add them to corp_df.
+    logger.info(f"Calculating pagerank scores for {len(corp_df)} documents.")
+    pagerank_df = Rank.get_pr_docs(corpus_docs)
+    corp_df = corp_df.merge(pagerank_df)
+
+    # Generate popular documents and merge into corp_df.
+    logger.info("Generating popular docs")
+    pop_docs = generate_pop_docs(prod_df, corp_df)
+    corp_df = corp_df.merge(pop_docs, on="id", how="outer")
+
+    logger.info(f"Saving corpus meta to {out_dir}")
+    corp_df.to_csv(join(out_dir, "corpus_meta.csv"))
+
+
+def create_corpus(directory, logger, nlp=None):
+    """Load corpus files from the given directory and format them as needed 
+    generating feature documents.
+
+    Args:
+        directory (str): Path to directory of corpus (JSON) files.
+        logger (logging.Logger)
+        nlp (spacy.lang.en.English or None, optional): Spacy language model.
+            If None, uses `en_core_web_md`. Default is None.
+
+    Returns:
+        list of dict: Corpus documents as a list of dictionaries with keys: 
+            `id`, `doc_id`, `doc_num`, `text`, `keywords`, `orgs`, 
+            `text_length`, and `ref_list`.
+    """
+    if nlp is None:
+        nlp = en_core_web_md.load()
+
+    common_orgs = load_common_orgs()
+    data = []
+    docs = [load_json(path) for path in glob(join(directory, "*json"))]
+
+    # Only keep documents that have the required keys.
+    required_keys = [
+        "id",
+        "doc_type",
+        "doc_num",
+        "text",
+        "keyw5",
+        "page_count",
+        "ref_list"
+    ]
+    orig_num_docs = len(docs)
+    docs = [doc for doc in docs if all(key in doc for key in required_keys)]
+    new_num_docs = len(docs)
+    if orig_num_docs < new_num_docs:
+        logger.warning(
+            f"{orig_num_docs - new_num_docs} documents were skipped because "
+            f"they were missing at least 1 key from {required_keys}."
+        )
+
+    # Normalize text lengths.
+    text_lengths = [
+        len(preprocess(doc["text"] / doc["page_count"], remove_stopwords=True))
+        for doc in docs
+    ]
+    min_length = min(text_lengths)
+    max_length = max(text_lengths)
+    text_lengths = [
+        (doc_length - min_length) / (max_length - min_length)
+        if doc_length != 0
+        else 0.0001
+        for doc_length in text_lengths
+    ]
+
+    for i in range(len(docs)):
+        doc = docs[i]
+        # Entity extraction isn't supported for large texts.
+        text = doc["text"][:999999]
+        # TODO: Is spacy model the best option here? It may be more efficient
+        # to use string or regex search since we're only keeping values that
+        # exist in common_entities anyway.
+        entities = [
+            ent.text
+            for ent in doc(text).ents
+            if ent.label_ == "ORG" and ent.text in common_orgs
+        ]
+        data.append(
+            {
+                "id": doc["id"],
+                "doc_id": " ".join([doc["doc_type"], doc["doc_num"]]),
+                "keywords": doc["keyw5"],
+                "orgs": dict(Counter(entities).most_common()),
+                "text_length": text_lengths[i],
+                "ref_list": doc["ref_list"],
+            }
+        )
+
+    return data
+
+
+def load_common_orgs():
+    """Load the file that contains common "ORG" entities.
+
+    Returns:
+        list
+    """
+    orgs = read_csv(
+        join(DATA_PATH, "features", "generated_files", "common_orgs.csv")
     )
-    r = rank.Rank()
-    day_delta = 80
-    d = datetime.timedelta(days=day_delta)
-    from_date = today - d
+    orgs = list(orgs["org"])
 
-    # TELEMETRY
-    # tele_df = mt.get_telemetry(day_delta)
-    # kw_doc_pairs = mt.parse_onDocOpen(tele_df)
-    # kw_doc_df = pd.DataFrame(kw_doc_pairs)
-
-    # SEARCH LOGS
-    # resp = meta.get_searchLogs(str(from_date.date()))
-
-    # until we get connection to prod data
-    logger.info(f"****    Reading in prod data from {prod_data}")
-    resp = pd.read_csv(prod_data)
-    logger.info("****    Getting top keywords")
-    popular_keywords = meta.get_top_keywords(resp)
-    logger.info("****    Making meta df")
-    meta_df = meta.scored_logs(resp)
-
-    # CORPUS
-    logger.info("****    Getting corpus data")
-    corp_df = r._getCorpusData(corpus_dir)
-    logger.info("****    Getting pagerank docs")
-    pr_df = r.get_pr_docs(corpus_dir)
-    logger.info("****    Merging corpus and pagerank df")
-    corp_df = corp_df.merge(pr_df)
-
-    logger.info("****    Generating popular docs")
-    docCounts = generate_pop_docs(popular_keywords, corp_df)
-    corp_df = corp_df.merge(docCounts, on="id", how="outer")
-
-    logger.info("****    Filling nulls/calculating kw in doc scores")
-    corp_df["kw_in_doc_score"].fillna(0.00001, inplace=True)
-    corp_df["kw_in_doc_score"] = (
-        corp_df["kw_in_doc_score"] - corp_df["kw_in_doc_score"].min()
-    ) / (corp_df["kw_in_doc_score"].max() - corp_df["kw_in_doc_score"].min())
-    corp_df.kw_in_doc_score.loc[corp_df.kw_in_doc_score == 0] = 0.00001
-    logger.info(f"****    Saving corpus meta to {out_dir}")
-    corp_df.to_csv(os.path.join(out_dir, "corpus_meta.csv"))
+    return orgs
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate Features CSV")
+    parser = ArgumentParser(description="Generate Features CSV")
     parser.add_argument(
         "--corpus", "-c", dest="corpus_dir", help="corpus directory, full path"
     )
@@ -122,25 +214,23 @@ if __name__ == "__main__":
         default=80,
         help="days of data to grab since todays date",
     )
-    # Until we can pull data from postgres from production automatically (currently avail in dev)
+    # Until we can pull data from postgres from production automatically 
+    # (currently avail in dev)
     parser.add_argument(
         "--prod",
         "-p",
         dest="prod_data",
-        default=os.path.join(
+        default=join(
             DATA_PATH,
-            "features", "generated_files", "prod_test_data.csv",
+            "features",
+            "generated_files",
+            "prod_test_data.csv",
         ),
         help="production data historical search logs csv ",
     )
-
-    # parser.add_argument(
-    #    "--outfile", "-o", dest="outfile", help="generated outfile name"
-    # )
-
     args = parser.parse_args()
     corpus_dir = args.corpus_dir
     days = args.day_delta
     prod_data = args.prod_data
-    # outfilename = args.outfile
-    # generate_ft_doc(corpus_dir=corpus_dir, days=days, prod_data=prod_data)
+
+    generate_ft_doc(corpus_dir, days, prod_data)
