8dbcf68980c463c7b88153e0c9d00f8b7ca97001
Olivia Kroop <kroop_olivia@bah.com>
2022-12-21 11:06:55 -0800

    rm unused function: cos_sim()

--- gamechangerml/src/utilities/test_utils.py
+++ gamechangerml/src/utilities/test_utils.py
@@ -144,31 +144,6 @@ def handle_sent_evals(index_path):
         logger.warning(e)
         return collect_evals(index_path)
 
-# from sentence_transformers==2.0.0
-# https://github.com/UKPLab/sentence-transformers/blob/master/sentence_transformers/util.py
-
-
-def cos_sim(a, b):
-    """
-    Computes the cosine similarity cos_sim(a[i], b[j]) for all i and j.
-    :return: Matrix with res[i][j]  = cos_sim(a[i], b[j])
-    """
-    if not isinstance(a, torch.Tensor):
-        a = torch.tensor(a)
-
-    if not isinstance(b, torch.Tensor):
-        b = torch.tensor(b)
-
-    if len(a.shape) == 1:
-        a = a.unsqueeze(0)
-
-    if len(b.shape) == 1:
-        b = b.unsqueeze(0)
-
-    a_norm = torch.nn.functional.normalize(a, p=2, dim=1)
-    b_norm = torch.nn.functional.normalize(b, p=2, dim=1)
-    return torch.mm(a_norm, b_norm.transpose(0, 1))
-
 
 def update_dictionary(old_dict, new_additions, prefix):
     '''Update master dictionary of unique queries'''
