30985fbdc840b86bad03fc25f5a6424ae1cbcb70
Olivia Kroop <kroop_olivia@bah.com>
2023-01-06 12:27:27 -0800

    initial upload of gamechangerml/src/search/document_comparison

---
+++ gamechangerml/src/search/document_comparison/__init__.py
@@ -0,0 +1,1 @@
+from .document_comparison import DocumentComparison
---
+++ gamechangerml/src/search/document_comparison/document_comparison.py
@@ -0,0 +1,34 @@
+"""Class definition for DocumentComparison, which is used in the ML API 
+/documentCompare endpoint."""
+
+from gamechangerml.configs import DocumentComparisonConfig
+from ..semantic_search import SemanticSearch
+
+
+class DocumentComparison(SemanticSearch):
+    def __init__(
+        self,
+        model_path,
+        index_directory_path,
+        load_index_from_file,
+        logger,
+        use_gpu,
+        threshold,
+    ):
+        super().__init__(
+            model_path,
+            index_directory_path,
+            load_index_from_file,
+            logger,
+            use_gpu,
+            threshold,
+        )
+
+    def get_default_threshold(self) -> float:
+        return DocumentComparisonConfig.DEFAULT_THRESHOLD_FLOAT
+
+    def get_score_display_map(self) -> dict:
+        return DocumentComparisonConfig.SCORE_DISPLAY_MAP
+
+    def should_include_results_below_threshold(self) -> bool:
+        return DocumentComparisonConfig.INCLUDE_RESULTS_BELOW_THRESHOLD
