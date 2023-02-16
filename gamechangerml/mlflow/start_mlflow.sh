1c406a14134961d521c0fc3b17fbb88dded3a14f
Jared Ross <Ross_Jared@bah.com>
2023-02-16 14:52:47 -0500

    getting rid of local changes to test

--- gamechangerml/mlflow/start_mlflow.sh
+++ gamechangerml/mlflow/start_mlflow.sh
@@ -3,5 +3,5 @@ set -e
 mkdir -p $FILE_DIR && mlflow server \
       --backend-store-uri file://${FILE_DIR} \
       --default-artifact-root s3://${AWS_BUCKET}/artifacts \
-
-
+      --host 0.0.0.0 \
+      --port 5050
